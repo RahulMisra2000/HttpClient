e274b1bb5c79f8343bc517d647edd5c62f034d3f
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-06 15:04:58 -0400

    Comment - HttpClient Error Handling code
    ErrorObservable.create(an instance of my custom object that we will populate with error info)

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -21,32 +21,42 @@ export class DataService {
     this.mostPopularBook = popularBook;
   }
 
+  // Demo code to get hard coded data when not using HttpClient API
   getAllReaders(): Reader[] {
-    // URL to get all readers is /api/readers
-    return allReaders;
+     return allReaders;
   }
 
+  // Demo code to get hard coded data when not using HttpClient API
   getReaderById(id: number): Reader {
-    // sample URL to get a reader by ID is /api/readers/1
+    
     return allReaders.find(reader => reader.readerID === id);
   }
 
+  
+  
+  // URL  /api/books
+  // Note: this api can return one of 2 kinds of observables
   getAllBooks(): Observable<Book[] | BookTrackerError> {
     console.log('Getting all books from the server.');
     return this.http.get<Book[]>('/api/books')
       .pipe(
-        catchError(err => this.handleHttpError(err))
+        // When there is an error, the system calls the function we provide inside the .catchError()
+        // and gives it an HttpErrorResponse object for our use to extract error information out of
+        catchError(err => this.x(err))  
       );
   }
 
-  private handleHttpError(error: HttpErrorResponse): Observable<BookTrackerError> {
+  private x(error: HttpErrorResponse): Observable<BookTrackerError> {
     let dataError = new BookTrackerError();
     dataError.errorNumber = 100;
     dataError.message = error.statusText;
     dataError.friendlyMessage = 'An error occurred retrieving data.';
     return ErrorObservable.create(dataError);
   }
+  
+  
 
+  // URL  e.g. /api/books/5
   getBookById(id: number): Observable<Book> {
     return this.http.get<Book>(`/api/books/${id}`, {
       headers: new HttpHeaders({
@@ -59,7 +69,7 @@ export class DataService {
   getOldBookById(id: number): Observable<OldBook> {
     return this.http.get<Book>(`/api/books/${id}`)
       .pipe(
-        map(b => <OldBook>{
+        map(b => <OldBook>{     // TYPE ASSERTION, what is called Type Casting in some languages
           bookTitle: b.title,
           year: b.publicationYear
         }),
@@ -67,6 +77,7 @@ export class DataService {
       );
   }
 
+  // URL  /api/books
   addBook(newBook: Book): Observable<Book> {
     return this.http.post<Book>('/api/books', newBook, {
       headers: new HttpHeaders({
@@ -75,6 +86,7 @@ export class DataService {
     });
   }
 
+  // URL  e.g. /api/books/5
   updateBook(updatedBook: Book): Observable<void> {
     return this.http.put<void>(`/api/books/${updatedBook.bookID}`, updatedBook, {
       headers: new HttpHeaders({
@@ -83,6 +95,7 @@ export class DataService {
     });
   }
 
+  // URL  e.g. api/books/5
   deleteBook(bookID: number): Observable<void> {
     return this.http.delete<void>(`/api/books/${bookID}`);
   }
